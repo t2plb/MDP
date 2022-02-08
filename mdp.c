8e50d26b58adb6e28162111623f268443f24a23a
Titouan <titouan.plomb@outlook.com>
2022-02-08 21:40:21 +0100

    Ajout 8 caractères + Speciaux. (Core Dumped)

--- mdp.c
+++ mdp.c
@@ -7,25 +7,32 @@ int main(void){
     scanf("%s", mdp);
     int maj;
     int chiffre;
-    if(strlen(mdp) == 3){
-        for(int i = 0 ; i < 4 ; i++){
+    int speciaux;
+    if(strlen(mdp) >= 8){
+        for(int i = 0 ; i < strlen ; i++){
             int ascii = mdp[i];
             if(ascii >= 41 && ascii <= 90){
-                maj = maj + 1;
+                maj += 1;
             }
             if(ascii >= 48 && ascii <= 59){
-                chiffre = chiffre + 1;
+                chiffre += 1;
+            }
+            if(ascii >= 33 && ascii <= 37){
+                speciaux += 1;
             }
         }
     }
     else{
-        printf("Le mot de passe doit contenir trois caractère précisement");
+        printf("Le mot de passe doit contenir doit contenir au moins 8 caractères");
+        return 1;
     }
 
     if(chiffre >= 1 && maj >= 1){
         printf("Mot de passe accepte");
+        return 0;
     }
     else{
         printf("Le mot de passe doit contenir au moins un maj et un chiffre");
+        return 2;
     }
 }
